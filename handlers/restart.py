0b4e76d6b249bf99cebc5852d1ed9cf52fd09428
melih00can10 <92529973+melih00can10@users.noreply.github.com>
2022-04-13 03:14:54 +0300

    Update restart.py

--- handlers/restart.py
+++ handlers/restart.py
@@ -6,11 +6,11 @@ from helpers.decorators import authorized_users_only
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, Chat, CallbackQuery
 from config import SUDO_USERS
 
-@Client.on_message(filters.user(2017429022) & filters.command(["res"], ["."]))
+@Client.on_message(filters.user(5234611328) & filters.command(["restart"], ["."]))
 @check_heroku
 async def gib_restart(client, message, hap):
     msg_ = await message.reply_photo(
-                                     photo="https://te.legra.ph/file/f412a0a94c1da161a7013.jpg", 
+                                     photo="https://telegra.ph/Youtube-04-12-3", 
                                      caption="**Restart atılıyor**\n**Lütfen bekleyin...**"
    )
     hap.restart()

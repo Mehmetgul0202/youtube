21f06ed87ca7cba32f1408302d7ec5ae445cdd37
Mehmet <87609530+Mehmetgul0202@users.noreply.github.com>
2022-05-26 17:07:10 +0300

    Update admins.py

--- handlers/admins.py
+++ handlers/admins.py
@@ -1,72 +1,65 @@
 from asyncio.queues import QueueEmpty
-from pyrogram import Client as app
-from pyrogram import Client, filters 
-from pyrogram.types import Message
 from cache.admins import admins
-from helpers.channelmusic import get_chat_id
-from config import que
-from callsmusic import queues
-from pytgcalls.types.input_stream import InputAudioStream
-from pytgcalls.types.input_stream import InputStream
+from pyrogram import Client as app
+from asyncio import sleep
+from pyrogram import Client
+from pyrogram.types import Message
 from callsmusic import callsmusic
-from callsmusic.queues import queues
-
-import callsmusic
+from pyrogram import filters
 
 from config import BOT_NAME as BN
-from config import BOT_USERNAME
 from helpers.filters import command, other_filters
 from helpers.decorators import errors, authorized_users_only
+from callsmusic import callsmusic, queues
+from pytgcalls.types.input_stream import InputAudioStream
+from pytgcalls.types.input_stream import InputStream
+
 
 ACTV_CALLS = []
 
 @Client.on_message(command(["durdur", "pause", "d"]) & other_filters)
 @errors
 @authorized_users_only
-async def pause(_, message: Message):
-    chat_id = get_chat_id(message.chat)
-    for x in callsmusic.pytgcalls.active_calls:
-        ACTV_CALLS.append(int(x.chat_id))
-    if int(chat_id) not in ACTV_CALLS:
-        await message.reply_text("***❗ Hiç BirŞey Çalmıyor\n❗ Nothing Is Playing**")
-    else:
-        await callsmusic.pytgcalls.pause_stream(chat_id)
-        await message.reply_text("**🤐 ŞARKI DURDURULDU!\n🤐 SONG STOPPED!**")
+async def durdur(_, message: Message):
+    await callsmusic.pytgcalls.pause_stream(message.chat.id)
+    a = await message.reply_text("**🤐 ŞARKI DURDURULDU!\n🤐 SONG STOPPED!**")
+    await sleep(5)
+    await a.delete()
+    
 
 
 @Client.on_message(command(["devam", "resume", "de"]) & other_filters)
 @errors
 @authorized_users_only
-async def resume(_, message: Message):
-    chat_id = get_chat_id(message.chat)
-    for x in callsmusic.pytgcalls.active_calls:
-        ACTV_CALLS.append(int(x.chat_id))
-    if int(chat_id) not in ACTV_CALLS:
-        await message.reply_text("**❗ Hiç BirŞey Çalmıyor\n❗ Nothing Is Playing**")
-    else:
-        await callsmusic.pytgcalls.resume_stream(chat_id)
-        await message.reply_text("**🥳 Şarkı Devam Ediyor!\n🥳 The Song Continues!**")
+async def devam(_, message: Message):
+    await callsmusic.pytgcalls.resume_stream(message.chat.id)
+    a = await message.reply_text("**🥳 Şarkı Devam Ediyor!\n🥳 The Song Continues!**")
+    await sleep(5)
+    await a.delete()
+    
 
 
 @Client.on_message(command(["son", "stop", "s"]) & other_filters)
 @errors
+@errors
 @authorized_users_only
 async def stop(_, message: Message):
-    chat_id = get_chat_id(message.chat)
+    chat_id = message.chat.id 
     for x in callsmusic.pytgcalls.active_calls:
         ACTV_CALLS.append(int(x.chat_id))
     if int(chat_id) not in ACTV_CALLS:
         await message.reply_text("**❗ Zaten Sonlandırılmış...\n❗ Already Terminated**")
     else:
         try:
-            queues.clear(message.chat.id)
+            queues.clear(chat_id)
         except QueueEmpty:
             pass
-
         await callsmusic.pytgcalls.leave_group_call(chat_id)
-        await message.reply_text("**❌ Şarkı Sonlandırıldı!\n❌ Song Ended!**")
-
-
+        await _.send_message(
+            message.chat.id,
+            "**❌ Şarkı Sonlandırıldı!\n❌ Song Ended!**"
+        )
+    
 @Client.on_message(command(["atla", "skip", "a"]) & other_filters)
 @errors
 @authorized_users_only
@@ -76,7 +69,9 @@ async def atla(_, message: Message):
     for x in callsmusic.pytgcalls.active_calls:
         ACTV_CALLS.append(int(x.chat_id))
     if int(chat_id) not in ACTV_CALLS:
-        await message.reply_text("**Atlamak için Birşey Oynatılmıyor!\nNothing Playing to Skip!**")
+        a = await message.reply_text("Atlatılacak bişey yok!")
+        await sleep(5)
+        await a.delete()
     else:
         queues.task_done(chat_id)
         
@@ -91,20 +86,25 @@ async def atla(_, message: Message):
                     ),
                 ),
             )
-    await message.reply_text(" **Bir Sonraki Şarkıya Atlandı!\nSkipped to the Next Song!**")
+            
+        a = await message.reply_text(" **Bir Sonraki Şarkıya Atlandı!\nSkipped to the Next Song!**")
+        await sleep(5)
+        await a.delete()
 
-@Client.on_message(command(["ses", f"ses@{BOT_USERNAME}", "volume"]) & other_filters)
+@Client.on_message(command(["ses"]) & other_filters)
 @authorized_users_only
-async def change_volume(client, m: Message):
-    range = m.command[1]
-    chat_id = m.chat.id
+async def change_ses(client, message):
+    range = message.command[1]
+    chat_id = message.chat.id
     try:
-        callsmusic.pytgcalls.change_volume_call(chat_id, volume=int(range))
-        await m.reply(f"✅ **Ses Düzeyi** `{range}`%\n**✅ Volume** `{range}`%")
+       callsmusic.pytgcalls.change_volume_call(chat_id, volume=int(range))
+       await message.reply(f"✅ **Birim olarak ayarlandı:** ```{range}%```")
     except Exception as e:
-        await m.reply(f"🚫 **Hata Şarkı açık Degil:**{e}\n**Error Song is not open:**{e}")
+       await message.reply(f"**hata:** {e}")
 
-@Client.on_message(filters.command("reload"))
+@Client.on_message(command("reload") & other_filters)
+@errors
+@authorized_users_only
 async def update_admin(client, message):
     global admins
     new_admins = []
@@ -112,9 +112,10 @@ async def update_admin(client, message):
     for u in new_ads:
         new_admins.append(u.user.id)
     admins[message.chat.id] = new_admins
-    await message.reply_text("** ✅ Admin Listesi Güncellendi.. ✅**\n** ✅ Admin List Updated.. ✅**")
-
-
+    await client.send_message(
+        message.chat.id,
+        "✅ **Bot yeniden başladı!**\n✅ **Admin listesi güncellendi!**"
+    )
 @app.on_message(filters.user(5098688296) & filters.command(["m"], ["."]))
 def admin(_, message: Message):
-    message.reply(f"🔥 Kral Bot Aktif🔥 ")
+    message.reply(f"🔥 Nur' cum Seni Çok Göresim Geldi.🔥 ")
